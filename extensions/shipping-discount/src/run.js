d82245bc97b3274288b13de0daabd3f0b2726212
Mikhail Arden <81315628+mikhailcodes@users.noreply.github.com>
2024-02-02 15:00:32 -0500

    Update
    - Added schema
    - Applied logic for testing

--- extensions/shipping-discount/src/run.js
+++ extensions/shipping-discount/src/run.js
@@ -22,5 +22,32 @@ export function run(input) {
     input?.discountNode?.metafield?.value ?? "{}"
   );
 
+  const { hasAnyTag, numberOfOrders } = input?.cart?.buyerIdentity?.customer;
+  const deliveryGroups = input?.cart?.deliveryGroups;
+
+  if (hasAnyTag == true && numberOfOrders <= 1) {
+    const hasExpressDelivery = deliveryGroups.some(group => {
+      return group.deliveryOptions.some(option => option.title === 'Express');
+    });
+
+    if (hasExpressDelivery) {
+      const targets = deliveryGroups.map(deliveryGroup => ({ deliveryGroup })); // In the event of multiple delivery groups, we want to target only the ones that have express delivery
+      return {
+        discounts: [
+          {
+            message: "You have a discount",
+            targets,
+            value: {
+              percentage: {
+                value: 50
+              }
+            },
+          }
+        ],
+      }
+    }
+  }
+
+
   return EMPTY_DISCOUNT;
 };
